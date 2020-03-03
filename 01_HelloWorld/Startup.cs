6b0b9118944c5b1b2596a35ee26563f9f704098f
Jakub Šenkýř <senkyr@spstrutnov.cz>
2020-03-03 11:58:54 +0100

    Ořezání aplikace o nepotřebné součásti

--- 01_HelloWorld/Startup.cs
+++ 01_HelloWorld/Startup.cs
@@ -50,7 +50,7 @@ namespace _01_HelloWorld
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=HelloWorld}/{action=Pozdrav}/{id?}");
             });
         }
     }
